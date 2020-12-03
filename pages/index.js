b0ab706dbcf915f2ed7104f0ef1e734ebe195993
Manikandan Sundararajan <mani@hey.com>
2020-12-03 02:51:58 -0500

    Load typeahead data from json file

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,35 @@
-import Head from 'next/head';
-import Nav from '@components/Nav';
-import Footer from '@components/Footer';
-import SearchBar from '@components/Search';
+import Head from "next/head";
+import Nav from "@components/Nav";
+import Footer from "@components/Footer";
+import SearchComponent from "@components/Search";
+import React, { useState, useEffect } from "react";
 
 export default function IndexPage() {
+	const [typeahead, setTypeahead] = useState([]);
+
+	useEffect(() => {
+		(async function loadData() {
+			let response = await fetch("/typeahead.json");
+
+			if (response.ok) {
+				console.log("Data is good");
+				let data = await response.json();
+				let authorsAndTags = data.authors.concat(data.tags);
+				console.log(authorsAndTags);
+			}
+		})();
+	}, []);
+
 	return (
 		<div>
 			<Head>
 				<title>Quotavio! Search for Quotes</title>
-				<link rel='icon' href='/favicon.ico' />
+				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Nav />
-			<main className='py-20 text-center'>
-				<h1 className='shadow-txt'>Search for a fun quote</h1>
-				<SearchBar />
+			<main className="py-20 text-center">
+				<h1 className="shadow-txt">Search for a fun quote</h1>
+				<SearchComponent data={typeahead} />
 			</main>
 			<Footer />
 		</div>
