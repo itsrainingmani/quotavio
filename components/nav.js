de929c13c25c5b8c5127a1d88eab039f782533a3
Manikandan Sundararajan <mani@hey.com>
2020-12-04 05:47:14 -0500

    Restore former styling

--- components/nav.js
+++ components/nav.js
@@ -11,7 +11,7 @@ export default function Nav() {
 			<ul className="flex items-center justify-between p-8">
 				<li>
 					<Link href="/">
-						<a className="text-blue-500 px-4 py-2 rounded shadow no-underline font-mono font-bold italic text-xl bg-gray-200 active:shadow-inner active:bg-gray-300">
+						<a className="text-blue-500 px-4 py-2 rounded hover:shadow no-underline font-mono font-bold italic text-xl hover:bg-gray-200 active:shadow-inner active:bg-gray-300">
 							Quotavio
 						</a>
 					</Link>
