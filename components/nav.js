0e7af7c98c6cbdf5606b5734890df192645b44cd
Manikandan Sundararajan <mani@hey.com>
2020-12-06 22:54:59 -0500

    Add about to navbar

--- components/nav.js
+++ components/nav.js
@@ -1,23 +1,32 @@
 import Link from "next/link";
 
-const links = [
+const extLinks = [
 	{ href: "https://github.com/itsrainingmani", label: "GitHub" },
 	// { href: 'https://nextjs.org/docs', label: 'Docs' },
+];
+
+const internalLinks = [
+	{ href: "/", label: "Quotavio" },
+	{ href: "/about", label: "About" },
 ];
 
 export default function Nav() {
 	return (
 		<nav>
 			<ul className="flex items-center justify-between p-8">
-				<li>
-					<Link href="/">
-						<a className="text-blue-500 px-4 py-2 rounded hover:shadow no-underline font-inter font-bold italic text-xl hover:bg-gray-200 active:shadow-inner active:bg-gray-300">
-							Quotavio
-						</a>
-					</Link>
-				</li>
+				<ul className="flex items-center justify-between space-x-2">
+					{internalLinks.map(({ href, label }) => (
+						<li key={`${href}${label}`}>
+							<Link href={href}>
+								<a className="text-blue-500 px-4 py-2 rounded hover:shadow no-underline font-inter font-bold italic text-xl hover:bg-gray-200 active:shadow-inner active:bg-gray-300">
+									{label}
+								</a>
+							</Link>
+						</li>
+					))}
+				</ul>
 				<ul className="flex items-center justify-between space-x-4">
-					{links.map(({ href, label }) => (
+					{extLinks.map(({ href, label }) => (
 						<li key={`${href}${label}`}>
 							<a href={href} className="no-underline btn-blue font-inter">
 								{label}
