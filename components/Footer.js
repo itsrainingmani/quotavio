a482f3c605d00d930bc0986a9843cb466530653f
Manikandan Sundararajan <mani@hey.com>
2020-12-06 22:55:34 -0500

    Change footer font to Inter

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<>
-			<footer className='fixed bottom-0 w-full h-5 flex justify-center items-center font-mono p-4 italic text-gray-600'>
+			<footer className="fixed bottom-0 w-full h-5 flex justify-center items-center font-inter p-4 italic text-gray-600">
 				Made with 💖 for you
 			</footer>
 		</>
