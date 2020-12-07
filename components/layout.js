a8f98e407874b0792a379d06be70a00ca7b58a2f
Manikandan Sundararajan <mani@hey.com>
2020-12-06 22:55:26 -0500

    Create layout component to share layout across site

---
+++ components/layout.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Nav from "@components/Nav";
+import Footer from "@components/Footer";
+import Link from "next/link";
+
+const siteTitle = "Quotavio! Search for Quotes";
+
+export default function Layout({ children }) {
+	return (
+		<div>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta
+					name="description"
+					content="Search for Quotes from authors with Quotavio"
+				/>
+				<meta name="og:title" content={siteTitle} />
+			</Head>
+			<Nav />
+			<main className="text-center max-w-3xl mx-auto pt-20">{children}</main>
+			<Footer />
+		</div>
+	);
+}
